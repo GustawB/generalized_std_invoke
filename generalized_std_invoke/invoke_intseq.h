6bdf6f0e7ece2ba2e4fc039059c93cb238777bcd
GustawB <gustaw.blachowski@gmail.com>
2023-11-25 22:10:12 +0100

    Early prototype of the return type.

--- generalized_std_invoke/invoke_intseq.h
+++ generalized_std_invoke/invoke_intseq.h
@@ -1,5 +1,14 @@
 #ifndef INVOKE_INTSEQ_H
 #define INVOKE_INTSEQ_H
 
+template<class F, class... ArgTypes>
+class result_type;
+
+template<class ...Args>
+bool isThereAnyIntegerSequencePassed
+
+template <class F, class... Args>
+constexpr result_type<class F, Args...>
+	invoke_intseq(F&& f, Args&&... args);
 
 #endif // !INVOKE_INTSEQ_H
