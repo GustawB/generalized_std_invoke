aba86e2d8ae0be71c590cfd8a7053ac97466fdba
GustawB <93836926+GustawB@users.noreply.github.com>
2023-11-25 22:51:42 +0100

    Implemented smple compile_time type check ffor std::integer_sequence.

--- generalized_std_invoke/invoke_intseq.h
+++ generalized_std_invoke/invoke_intseq.h
@@ -1,11 +1,25 @@
 #ifndef INVOKE_INTSEQ_H
 #define INVOKE_INTSEQ_H
 
+#include <type_traits>
+#include <utility>
+
 template<class F, class... ArgTypes>
 class result_type;
 
 template<class ...Args>
-bool isThereAnyIntegerSequencePassed
+bool isThereAnyIntegerSequencePassed(Args... args)
+{
+	//The idea is that the fold expression will take care of iterating
+	//through the args list, and that the lambda will perform the type check
+	//on every element (x). I hope that combining a lambda with templates
+	//andstd::is_same will produce a code that is evaluated
+	//at the compilation time.
+	return (... || [](auto x))
+	{
+		return (std::is_same(std::integer_sequence, decltype(x)));
+	}
+}
 
 template <class F, class... Args>
 constexpr result_type<class F, Args...>
