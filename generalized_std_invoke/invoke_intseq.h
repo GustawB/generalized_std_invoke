40c1ffba6b74c22e035d90de85c996cd566c344a
GustawB <93836926+GustawB@users.noreply.github.com>
2023-11-26 12:02:04 +0100

    Add behaviour triggered when there was no integer sequence passed (just call std::invoke).

--- generalized_std_invoke/invoke_intseq.h
+++ generalized_std_invoke/invoke_intseq.h
@@ -3,26 +3,34 @@
 
 #include <type_traits>
 #include <utility>
+#include <array>
 
-template<class F, class... ArgTypes>
-class result_type;
-
-template<class ...Args>
-bool isThereAnyIntegerSequencePassed(Args... args)
+template<class... Args>
+bool wasThereAnyIntegerSequencePassed(Args&&... args)
 {
-	//The idea is that the fold expression will take care of iterating
-	//through the args list, and that the lambda will perform the type check
-	//on every element (x). I hope that combining a lambda with templates
-	//andstd::is_same will produce a code that is evaluated
-	//at the compilation time.
-	return (... || [](auto x))
+	// The idea is that the fold expression will take care of iterating
+	// through the args list, and that the lambda will perform the type check
+	// on every element (x). I hope that combining a lambda with templates
+	// and std::is_same will produce a code that is evaluated
+	// at the compilation time.
+	return (... || [](auto&& x))
 	{
 		return (std::is_same(std::integer_sequence, decltype(x)));
 	}
 }
 
 template <class F, class... Args>
-constexpr result_type<class F, Args...>
-	invoke_intseq(F&& f, Args&&... args);
+constexpr auto invoke_intseq(F&& f, Args&&... args) -> decltype(auto)
+{// Delay the type deduction to the end of the function execution. 
+	if constexpr (!wasThereAnyIntegerSequencePassed(std::forward(args...)))
+	{
+		// There was no integer sequence passed, so we can just use std::invoke.
+		return std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+	}
+	else
+	{
+		// TODO: recursive bullshit
+	}
+}
 
 #endif // !INVOKE_INTSEQ_H
