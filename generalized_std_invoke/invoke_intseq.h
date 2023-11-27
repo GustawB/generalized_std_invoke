1db65ff4e5c30d2335e4228e7fd1f91517c56aba
GustawB <93836926+GustawB@users.noreply.github.com>
2023-11-27 19:48:56 +0100

    Some fixes, I know nothing.

--- generalized_std_invoke/invoke_intseq.h
+++ generalized_std_invoke/invoke_intseq.h
@@ -11,6 +11,13 @@
 // https://en.cppreference.com/w/cpp/utility/functional/invoke
 namespace detail
 {
+	template <class F>
+	concept voidReturn = requires()
+	{
+		//Aparently doesn;t work, We need a type of every variable in the Args...
+		std::is_same<std::invoke_result_t<F>, void>::value == true;
+	};
+
 	template <class type_to_check, class Arg>
 	constexpr bool compareTypes(Arg&& arg)
 	{
@@ -21,18 +28,19 @@ namespace detail
 	constexpr bool wasThereAnyIntegerSequencePassed(Args&&... args)
 	{
 		//return std::is_same<type_to_check, decltype(args)>::value;
-		return (... && compareTypes(std::forward(args)));
+		return (... && compareTypes(std::forward<Args>(args)));
 	}
 
 	// Function used to recursively perform all calls of F.
-	template <class result_type, class F, class... Args>
-	constexpr auto invoke_all(int iter) -> decltype(auto)
+	template <class F, class... Args>
+	constexpr auto invoke_all() -> decltype(auto)
 	{
-		()
+		
 	}
 
 	// invoke_all specialization for when the return type of F is void.
-	template <void, class F, class... Args>
+	template <class F, class... Args>
+	requires voidReturn<F> // Concept that reuires this partial specialization to have a F function that returns void.
 	constexpr auto invoke_all() -> decltype(auto)
 	{
 
@@ -40,18 +48,16 @@ namespace detail
 } // namespace detail
 
 template <class F, class... Args>
-constexpr result_type invoke_intseq(F&& f, Args&&... args)
+constexpr auto invoke_intseq(F&& f, Args&&... args) -> decltype(auto)
 {// Delay the type deduction to the end of the function execution. 
-	if constexpr (!detail::wasThereAnyIntegerSequencePassed<std::integer_sequence, Args...>(std::forward(args...)))
+	if constexpr (!detail::wasThereAnyIntegerSequencePassed<std::integer_sequence, Args>(std::forward<Args>(args...)))
 	{
-		std::cout << Args[i];
 		// There was no integer sequence passed, so we can just use std::invoke.
-		return std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+		return std::invoke(std::forward<F>(f), std::forward(args)...);
 	}
 	else
 	{
 		// TODO: recursive bullshit
-		std::invoke_result_t<F> result_type;
 	}
 }
 
