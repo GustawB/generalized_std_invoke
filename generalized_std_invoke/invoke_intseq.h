e53aa7ce1beb4d20188d530e6eaa65b5edc165bb
GustawB <gustaw.blachowski@gmail.com>
2023-11-28 09:49:18 +0100

    something something merge conflict

--- generalized_std_invoke/invoke_intseq.h
+++ generalized_std_invoke/invoke_intseq.h
@@ -2,30 +2,58 @@
 #define INVOKE_INTSEQ_H
 
 #include <type_traits>
+#include <iostream>
 #include <utility>
 #include <array>
 
-template<class... Args>
-bool wasThereAnyIntegerSequencePassed(Args&&... args)
+// Namespace containing all helper functions, naming based on the
+// std::invoke documentation available here:
+// https://en.cppreference.com/w/cpp/utility/functional/invoke
+namespace detail
 {
-	// The idea is that the fold expression will take care of iterating
-	// through the args list, and that the lambda will perform the type check
-	// on every element (x). I hope that combining a lambda with templates
-	// and std::is_same will produce a code that is evaluated
-	// at the compilation time.
-	return (... || [](auto&& x))
+	template <class F>
+	concept voidReturn = requires()
 	{
-		return (std::is_same(std::integer_sequence, decltype(x)));
+		//Aparently doesn;t work, We need a type of every variable in the Args...
+		std::is_same<std::invoke_result_t<F>, void>::value == true;
+	};
+
+	template <class type_to_check, class Arg>
+	constexpr bool compareTypes(Arg&& arg)
+	{
+		return std::is_same<type_to_check, decltype(arg)>::value;
 	}
-}
+
+	template<class type_to_check, class... Args>
+	constexpr bool wasThereAnyIntegerSequencePassed(Args&&... args)
+	{
+		//return std::is_same<type_to_check, decltype(args)>::value;
+		return (... && compareTypes(std::forward<Args>(args)));
+	}
+
+	// Function used to recursively perform all calls of F.
+	template <class F, class... Args>
+	constexpr auto invoke_all() -> decltype(auto)
+	{
+
+	}
+
+	// invoke_all specialization for when the return type of F is void.
+	template <class F, class... Args>
+	requires voidReturn<F> // Concept that reuires this partial specialization to have a F function that returns void.
+		constexpr auto invoke_all() -> decltype(auto)
+	{
+
+	}
+} // namespace detail
 
 template <class F, class... Args>
 constexpr auto invoke_intseq(F&& f, Args&&... args) -> decltype(auto)
 {// Delay the type deduction to the end of the function execution. 
-	if constexpr (!wasThereAnyIntegerSequencePassed(std::forward(args...)))
+	if constexpr (!detail::wasThereAnyIntegerSequencePassed<std::integer_sequence, Args>(std::forward<Args>(args...)))
 	{
 		// There was no integer sequence passed, so we can just use std::invoke.
-		return std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+		return std::invoke(std::forward<F>(f), std::forward(args)...);
 	}
 	else
 	{
