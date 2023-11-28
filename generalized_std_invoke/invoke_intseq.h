fbf9d3d01110d79519b0f76c7dec3b4fd000d80f
GustawB <gustaw.blachowski@gmail.com>
2023-11-28 13:55:14 +0100

    YYY?

--- generalized_std_invoke/invoke_intseq.h
+++ generalized_std_invoke/invoke_intseq.h
@@ -3,21 +3,16 @@
 
 #include <type_traits>
 #include <iostream>
+#include <functional>
 #include <utility>
 #include <array>
+#include <tuple>
 
 // Namespace containing all helper functions, naming based on the
 // std::invoke documentation available here:
 // https://en.cppreference.com/w/cpp/utility/functional/invoke
 namespace detail
 {
-	template <class F>
-	concept voidReturn = requires()
-	{
-		//Aparently doesn;t work, We need a type of every variable in the Args...
-		std::is_same<std::invoke_result_t<F>, void>::value == true;
-	};
-
 	template <class type_to_check, class Arg>
 	constexpr bool compareTypes(Arg&& arg)
 	{
@@ -27,36 +22,38 @@ namespace detail
 	template<class type_to_check, class... Args>
 	constexpr bool wasThereAnyIntegerSequencePassed(Args&&... args)
 	{
-		//return std::is_same<type_to_check, decltype(args)>::value;
 		return (... && compareTypes(std::forward<Args>(args)));
 	}
 
 	// Function used to recursively perform all calls of F.
 	template <class F, class... Args>
-	constexpr auto invoke_all() -> decltype(auto)
+	struct invoke
 	{
-
-	}
-
-	// invoke_all specialization for when the return type of F is void.
-	template <class F, class... Args>
-	requires voidReturn<F> // Concept that reuires this partial specialization to have a F function that returns void.
-		constexpr auto invoke_all() -> decltype(auto)
-	{
-
-	}
+		static constexpr void run()
+		{
+			//I guess there are no more params, only function, so we can invoke it. 
+		}
+	};
 } // namespace detail
 
 template <class F, class... Args>
 constexpr auto invoke_intseq(F&& f, Args&&... args) -> decltype(auto)
 {// Delay the type deduction to the end of the function execution. 
-	if constexpr (!detail::wasThereAnyIntegerSequencePassed<std::integer_sequence, Args>(std::forward<Args>(args...)))
+	if constexpr (!detail::wasThereAnyIntegerSequencePassed<std::integer_sequence, Args...>(std::forward<Args...>(args...)))
 	{
 		// There was no integer sequence passed, so we can just use std::invoke.
 		return std::invoke(std::forward<F>(f), std::forward(args)...);
 	}
 	else
 	{
+		std::tuple<Args...> argsTuple;
+		for (size_t i = 0; i < std::tuple_size<decltype(argsTuple)>::value; ++i)
+		{
+			if constexpr (std::is_same<decltype(std::get<i>(argsTuple)), std::integer_sequence>)
+			{
+
+			}
+		}
 		// TODO: recursive bullshit
 	}
 }
